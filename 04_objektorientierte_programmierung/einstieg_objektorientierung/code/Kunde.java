0d949f45d87bd5852604b28ef64eb901da5589a0
trioxtron <trioxgames@gmail.com>
2022-02-17 21:16:45 +0100

    create composition of 'Kunde' and 'Konto' classes

--- 04_objektorientierte_programmierung/einstieg_objektorientierung/code/Kunde.java
+++ 04_objektorientierte_programmierung/einstieg_objektorientierung/code/Kunde.java
@@ -4,10 +4,11 @@ public class Kunde{
     private boolean weiblich;
     private Konto girokonto;
 
-    public Kunde(String name, boolean weiblich, Konto girokonto){
+    public Kunde(String name, boolean weiblich, int kontonummer){
         this.name = name;
         this.weiblich = weiblich;
-        this.girokonto = girokonto
+        
+        girokonto = new Konto(kontonummer);
     }
 
     public string getName(){
@@ -20,5 +21,19 @@ public class Kunde{
 
     public Konto getGirokonto(){
         return girokonto;
+    }
+
+    public void abheben(double betrag){
+        if(girokonto.getKontostand() >= betrag){
+            girokonto.auszahlen(betrag);
+        }
+    }
+
+    public void einzahlen(double betrag){
+        girokonto.einzahlen(betrag);
+    }
+
+    public void ueberweisen(double betrag, Kunde empfaenger){
+        girokonto.ueberweisen(betrag, empfaenger.getGirokonto());
     }
 }
